813eb6ca7cbe752492d7b45ad472c93dfb06369c
Kazuki Sawada <kazuki@6715.jp>
2019-07-01 23:13:24 +0900

    impl action: power

--- cnc/cnc.go
+++ cnc/cnc.go
@@ -5,4 +5,5 @@ import "github.com/labstack/echo/v4"
 func RegisterHandler(g *echo.Group) {
 	g.GET("/:host/state", getState)
 	g.PUT("/:host/state", putState)
+	g.POST("/:host/power", postPower)
 }
---
+++ cnc/power.go
@@ -0,0 +1,31 @@
+package cnc
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/kaz/flos-garden/common"
+	"github.com/labstack/echo/v4"
+)
+
+func postPower(c echo.Context) error {
+	var power string
+	if err := c.Bind(&power); err != nil {
+		return fmt.Errorf("failed to bind request body: %v\n", err)
+	}
+
+	resp, err := common.Request(http.MethodPost, c.Param("host"), "/power", power, nil)
+	if err != nil {
+		return fmt.Errorf("request failed: %v\n", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		var msg string
+		if err := common.ReadBody(resp, &msg); err != nil {
+			return fmt.Errorf("failed to decode resp body: %v\n", err)
+		}
+		return fmt.Errorf("failed to switch power: %v\n", msg)
+	}
+
+	return c.NoContent(http.StatusOK)
+}
