549d3ff6f867f070c903561804f7b9dfecbba282
Kazuki Sawada <kazuki@6715.jp>
2019-07-01 23:35:09 +0900

    re-impl: database handlers

--- database/handler.go
+++ database/handler.go
@@ -1,23 +1,42 @@
 package database
 
 import (
-	"io/ioutil"
+	"fmt"
 	"net/http"
+	"path/filepath"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
 
 func RegisterHandler(g *echo.Group) {
+	g.GET("/blob/:host/:id", getBLOB)
 	g.POST("/query", postQuery)
 }
 
-func postQuery(c echo.Context) error {
-	sql, err := ioutil.ReadAll(c.Request().Body)
+func getBLOB(c echo.Context) error {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 64)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to parse request body: %v\n", err)
 	}
 
-	rows, err := DB().Queryx(string(sql))
+	var path string
+	var data []byte
+	if err := DB().QueryRow("SELECT series, contents FROM bookshelf_data_archive WHERE host = ? AND remote_id = ?", c.Param("host"), id).Scan(&path, &data); err != nil {
+		return fmt.Errorf("failed to get BLOB: %v\n", err)
+	}
+
+	c.Response().Header().Set("Content-Disposition", fmt.Sprintf(`attachment; filename="%s"`, filepath.Base(path)))
+	return c.Blob(http.StatusOK, "application/octet-stream", data)
+}
+
+func postQuery(c echo.Context) error {
+	var sql string
+	if err := c.Bind(&sql); err != nil {
+		return fmt.Errorf("failed to bind request body: %v\n", err)
+	}
+
+	rows, err := DB().Queryx(sql)
 	if err != nil {
 		return err
 	}
