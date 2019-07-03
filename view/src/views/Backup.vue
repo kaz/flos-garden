9341b91c4eb6edde66d04b5797a66faf1602de31
Kazuki Sawada <kazuki@6715.jp>
2019-07-03 16:46:42 +0900

    improve backup view

--- view/src/views/Backup.vue
+++ view/src/views/Backup.vue
@@ -1,6 +1,11 @@
 <template>
   <section id="backup">
-    <h1>File Snapshots</h1>
+    <h1>Query</h1>
+    Host: <input type="text" v-model="query.host"><br>
+    File: <input type="text" v-model="query.file"><br>
+    <button @click="listBackups(1)">run query</button>
+
+    <h1>{{ count }} snapshots found</h1>
     <table>
       <thead>
         <tr>
@@ -19,6 +24,7 @@
         </tr>
       </tbody>
     </table>
+    <p>page <input type="number" v-model="page" @change="listBackups(page)"> / {{ pageLast }}</p>
   </section>
 </template>
 
@@ -31,6 +37,9 @@
   padding: .5em 2em;
   border: 1px solid #999;
 }
+#backup input[type=number] {
+  width: 3em;
+}
 </style>
 
 <script>
@@ -39,21 +48,49 @@
 export default {
   data() {
     return {
+      query: {
+        host: "%",
+        file: "%",
+      },
+
+      page: 1,
+      pageRows: 1000,
+
+      count: 0,
       snapshots: [],
     }
   },
+  computed: {
+    pageLast() {
+      return Math.ceil(this.count / this.pageRows);
+    },
+  },
   created() {
-    this.loadMonitors();
+    this.listBackups(1);
   },
   beforeDestroy() {
     clearInterval(this.timer);
   },
   methods: {
-    async loadMonitors() {
+    async listBackups(page) {
+      this.page = page;
+
+      const fromWhereOrder = `FROM bookshelf_data_archive WHERE host LIKE '${this.query.host}' AND series LIKE '${this.query.file}' ORDER BY created DESC`;
+
+      const cResp = await fetch(`/api/database/query`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(`SELECT COUNT(*) ${fromWhereOrder}`),
+      });
+      if(cResp.status != 200){
+        return alert(await cResp.text());
+      }
+      this.count = parseInt((await cResp.json()).rows[0][0]);
+
       const resp = await fetch(`/api/database/query`, {
         method: "POST",
         headers: {"Content-Type": "application/json"},
-        body: JSON.stringify("SELECT BINARY created, host, series, remote_id FROM bookshelf_data_archive ORDER BY created DESC"),
+        body: JSON.stringify(`SELECT BINARY created, host, series, remote_id ${fromWhereOrder} LIMIT ${this.pageRows*(this.page-1)}, ${this.pageRows}`),
       });
       if(resp.status != 200){
         return alert(await resp.text());
